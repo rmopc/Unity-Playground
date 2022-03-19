6812ac4d77ecec24471e382dd674a86d238e3e3f
Ronie Oljemark <ronie.oljemark@student.careeria.fi>
2022-03-19 17:01:26 +0200

    ovikoodipaneelin koodaus ja äänien asettaminen sekä toiminnan testaaminen.

---
+++ Assets/DisplayControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayControl : MonoBehaviour {
+
+	void Start () 
+	{ 
+			
+	}
+	
+	void Update () 
+	{
+		GetComponent<TextMesh>().text = ButtonClickController.playerCode;
+	}
+}
---
+++ Assets/_Scripts/ButtonClickController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClickController : MonoBehaviour {
+
+	public static string correctCode = "0016";
+	public static string playerCode = "";
+	public static int totalDigits = 0;
+
+	public GameObject objectToUnlock;
+	public AudioSource audioSource;
+	public AudioClip clickSound;
+	public AudioClip correctCodeSound;
+	public AudioClip wrongCodeSound;
+
+	void Start () 
+	{
+		
+	}
+	
+	
+	void Update () 
+	{
+		//Debug.Log(playerCode);
+
+		if (totalDigits == 4)
+        {
+			if (playerCode == correctCode)
+            {
+				Debug.Log("Code is Correct!");
+				audioSource.PlayOneShot(correctCodeSound, 0.25f);
+				objectToUnlock.GetComponent<RayCastDoor>().isLocked = false;
+				playerCode = "";
+				totalDigits = 0;
+			}
+
+            else
+            {
+				Debug.Log("Wrong code entered");
+				audioSource.PlayOneShot(wrongCodeSound, 0.25f);
+				playerCode = "";
+				totalDigits = 0;
+				
+            }
+        }
+	}
+
+	void OnMouseDown()
+    {
+		audioSource.PlayOneShot(clickSound, 0.25f);
+		playerCode += gameObject.name;
+		totalDigits += 1;
+    }
+}
